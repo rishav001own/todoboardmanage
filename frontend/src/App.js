f84ec3b737c5bc7267c22a007292c973ec2c92fd
rishav-sharma-vst-au4 <54503782+rishav-sharma-vst-au4@users.noreply.github.com>
2020-12-27 03:34:53 +0530

    setting up redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,36 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './components/pages/Landing';
 import './App.css';
 import Login from './components/pages/Login';
 import Register from './components/pages/Register'
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 const App = () => {
   return (
-    <Fragment>
-      <Landing />
-      <Login/>
-      <Register/>
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Route exact path='/' component={Landing} />
+        <Switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </Fragment>
+    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Route exact path='/' component={Landing} />
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 };
 
