a3ae20432ae9b951c295660e10e3a910457f5e35
rishav-sharma-vst-au4 <54503782+rishav-sharma-vst-au4@users.noreply.github.com>
2020-12-27 01:10:14 +0530

    backend auth,card ad board

--- routes/auth.js
+++ routes/auth.js
@@ -55,4 +55,15 @@ async (req,res) => {
     }
 })
 
+// Get authorized user
+router.get('/', auth, async (req, res) => {
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      res.json(user);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  });
+
 module.exports = router;
--- server.js
+++ server.js
@@ -27,7 +27,9 @@ app.use(express.json({extented:false}))
 
 //routes
 app.use('/auth',require('./routes/auth'));
-app.use('/user',require('./routes/users'))
+app.use('/user',require('./routes/users'));
+app.use('/boards',require('./routes/boards'));
+app.use('/cards',require('./routes/cards'))
 
 // SERVER LISTEN/START
 app.listen(port, () => {
