483043db17bf0fbb1b16f4c3c63a7a0174913cf0
rishav-sharma-vst-au4 <54503782+rishav-sharma-vst-au4@users.noreply.github.com>
2020-12-27 02:36:41 +0530

    formstyles

---
+++ frontend/src/components/pages/Login.js
@@ -0,0 +1,6 @@
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Copyright from './Copyright';
+import useStyles from '../utils/formStyles';
