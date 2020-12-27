21790589b10e53db3ba25a56d80f68c036c20c22
rishav-sharma-vst-au4 <54503782+rishav-sharma-vst-au4@users.noreply.github.com>
2020-12-27 05:38:50 +0530

    dash &board

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Login from './components/pages/Login';
 import Register from './components/pages/Register';
 import Dashboard from './components/pages/Dashboard';
+import Board from './components/pages/Board';
 import Navbar from './components/pages/Navbar';
 import Alert from './components/design/Alert';
 import { loadUser } from './actions/auth';
@@ -34,6 +35,7 @@ const App = () => {
             <Route exact path='/register' component={Register} />
             <Route exact path='/login' component={Login} />
             <Route exact path='/dashboard' component={Dashboard} />
+            <Route exact path='/board/:id' component={Board} />
           </Switch>
         </Fragment>
       </Router>
--- frontend/src/components/pages/Dashboard.js
+++ frontend/src/components/pages/Dashboard.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { Redirect, Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import CreateBoard from './CreateBoard';
 
-const Dashboard = () => {
-  return <div>Dashboard</div>;
+const Dashboard = ({ auth: { user, isAuthenticated } }) => {
+    const [boards, setBoards] = useState([]);
+  
+    useEffect(() => {
+      (async function getBoards() {
+        setBoards((await axios.get('/api/boards')).data);
+      })();
+    }, []);
+  
+    if (!isAuthenticated) {
+      return <Redirect to='/' />;
+    }
+  
+    return (
+      <section className='dashboard'>
+        <h1>Welcome {user && user.name}</h1>
+        <h2>Your Boards</h2>
+        <div className='boards'>
+          {boards.map((board) => (
+            <Link key={board._id} to={`/board/${board._id}`} className='board-card'>
+              {board.title}
+            </Link>
+          ))}
+          <CreateBoard/>
+        </div>
+      </section>
+    );
+  };
+
+
+Dashboard.propTypes = {
+  auth: PropTypes.object.isRequired,
 };
 
-export default Dashboard;
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps)(Dashboard);
