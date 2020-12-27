a234fb306afb06c22fc3346d68312716f7355586
rishav-sharma-vst-au4 <54503782+rishav-sharma-vst-au4@users.noreply.github.com>
2020-12-27 07:42:57 +0530

    minor bug fix

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,4 +1,6 @@
 import { combineReducers } from 'redux';
 import alert from './alert';
+import auth from './auth';
+import board from './board';
 
-export default combineReducers({alert});
+export default combineReducers({ alert, auth, board });
